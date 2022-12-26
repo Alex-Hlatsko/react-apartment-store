3ad410e6961970dd8b26908f9b113b3e79ba6d54
Alen-Berg <alexei.hlatsko@gmail.com>
2022-12-26 22:13:41 +0100

    content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
 
-const Header = () => {
+const Header = ({data}) => {
   return (
     <div className='header'>
-        <div className="header_bg"/>
+        <div className="header_bg" style={{backgroundImage: `url(${data.bg_img})`}}/>
         <div className="header_content flex flex-col justify-center">
-              <h1 className="header_title">Find the perfect place for you</h1>
-              <p className="header_subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem natus ullam obcaecati, voluptate cupiditate exercitationem perspiciatis distinctio earum!</p>
-              <Link to="/apartmants"><span className='btn'>More</span></Link>
+              <h1 className="header_title">{data.title}</h1>
+              <p className="header_subtitle">{data.subtitle}</p>
+              <Link to="/apartmants"><span className='btn'>{data.btn}</span></Link>
         </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import Navigation from '../components/Navigation/Navigation'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
+import Content from '../components/Content/Content'
 
 const Home = () => {
+  const dataHeader = {
+    bg_img: "./img/home.jpg",
+    title : "Find the perfect place for you",
+    subtitle : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem natus ullam obcaecati, voluptate cupiditate exercitationem perspiciatis distinctio earum!",
+    btn : "More"
+  }
+
+  const dataContent = {
+    img: "./img/home_content.jpg",
+    title : "Find a place for yourself and make your dreams come true",
+    subtitle : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus fugiat dolorum, incidunt suscipit unde, adipisci voluptate quo voluptatem inventore saepe eligendi numquam voluptas quisquam sequi placeat ratione tempora nihil accusantium ullam dolores.",
+    btn : "Show"
+  }
   return (
     <>
         <Navigation/>
-        <Header/>
+        <Header data={dataHeader}/>
+        <Content data={dataContent}/>
         <Footer/>
     </>
   )
